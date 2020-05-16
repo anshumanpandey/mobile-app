a500ab6abb2c07d5143882037bbb6703f93224c5
root <leo_9_t@hotmail.com>
2020-05-16 06:54:14 -0400

    ADDED typescript

--- App.tsx
+++ App.tsx
@@ -6,7 +6,8 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React from 'react'
+import { Component } from 'react'
 import {
   SafeAreaView,
   StyleSheet,
@@ -24,7 +25,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App: () => React.ReactNode = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -33,16 +34,11 @@ const App: () => React$Node = () => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
                 screen and then come back to see your edits.
               </Text>
             </View>
