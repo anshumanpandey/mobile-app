f698cd760c6da8907de4bfc658f0d2f80f2f1cab
leo <leo_9_t@hotmail.com>
2020-06-23 03:30:15 -0400

    FIX on typo

--- screens/home/CustomeDrawer.tsx
+++ screens/home/CustomeDrawer.tsx
@@ -38,18 +38,18 @@ const DrawerMenu = ({ navigation }: { navigation: any }) => {
         <>
             <View style={styles.container}>
                 <Layout style={{ width: '75%', display: 'flex', flexDirection: 'row', paddingBottom: '20%' }}>
-                    {!profile?.selfiurl ? (
+                    {profile?.selfiurl == "" && (
                         <Image
                             style={{ width: 80, height: 80, resizeMode: 'contain' }}
                             source={require('../../image/rightcars.png')}
                         />
-                    ) : null}
-                    {profile?.selfiurl ? (
+                    )}
+                    {profile?.selfiurl != "" && (
                         <Image
                             style={{ width: 80, height: 80, resizeMode: 'contain' }}
                             source={{ uri: `https://www.right-cars.com/uploads/selfi/${profile?.selfiurl}`}}
                         />
-                    ) : null}
+                    )}
                     <Layout style={{ marginLeft: '10%' }}>
                         <Text category='h3'>{profile?.username || profile?.email}</Text>
                         <Text onPress={() => navigation.navigate("EditProfile")} style={{ color: '#52e6fe' }} category='s1'>Edit profile</Text>
