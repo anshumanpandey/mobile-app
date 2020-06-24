eac4de11df82b37f77f07297c75adb47f72f6922
leo <leo_9_t@hotmail.com>
2020-06-24 07:50:48 -0400

    ADDRESS 2 is not mandatory

--- utils/userHasFullProfile.ts
+++ utils/userHasFullProfile.ts
@@ -8,7 +8,6 @@ export default (profile: {[k: string]: string}): boolean => {
     profile.lastname !== null && profile.lastname !== undefined && profile.lastname !== "" &&
     profile.mobilecode !== null && profile.mobilecode !== undefined && profile.mobilecode !== "" &&
     profile.add1 !== null && profile.add1 !== undefined && profile.add1 !== "" &&
-    profile.add2 !== null && profile.add2 !== undefined && profile.add2 !== "" &&
     profile.city !== null && profile.city !== undefined && profile.city !== "" &&
     profile.postcode !== null && profile.postcode !== undefined && profile.postcode !== "" &&
     profile.country !== null && profile.country !== undefined && profile.country !== "";
