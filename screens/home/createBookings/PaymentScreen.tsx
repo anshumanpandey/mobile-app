3d55a7f3e57b6cfcf526f2fb53caff9b0d9e851a
leo <leo_9_t@hotmail.com>
2020-06-29 17:16:04 -0400

    FIX on protocol for url address

--- screens/home/createBookings/PaymentScreen.tsx
+++ screens/home/createBookings/PaymentScreen.tsx
@@ -59,7 +59,7 @@ const GET_PAYPAL_JSON = (vehicle: VehVendorAvail, meta, extras: (PricedEquip & {
         }],
         "note_to_payer": "Contact us for any questions on your order.",
         "redirect_urls": {
-            "return_url": "http://right-cars.com/mobileapp/index.php?module_name=PAYMENT_SUCCESS",
+            "return_url": "https://right-cars.com/mobileapp/index.php?module_name=PAYMENT_SUCCESS",
             "cancel_url": "https://example.com"
         }
     }
