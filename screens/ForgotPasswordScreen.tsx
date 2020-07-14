89d93fff7816ebc9f05127091f971dfafe675abd
leo <leo_9_t@hotmail.com>
2020-07-14 00:44:29 -0400

    ADDED translation to Forgot password screen

--- screens/ForgotPasswordScreen.tsx
+++ screens/ForgotPasswordScreen.tsx
@@ -7,9 +7,13 @@ import BackButton from '../partials/BackButton';
 import useAxios from 'axios-hooks'
 import { GRCGDS_BACKEND } from 'react-native-dotenv'
 import { AppFontBold, AppFontRegular } from '../constants/fonts'
+import { useTranslation } from 'react-i18next';
+import { TRANSLATIONS_KEY } from '../utils/i18n';
 
 const DocumentScreen = () => {
   const navigation = useNavigation();
+  const { i18n } = useTranslation();
+
   const [email, setEmail] = useState<null | string>(null);
 
   const [{ data, loading, error }, doRecover] = useAxios({
@@ -23,8 +27,12 @@ const DocumentScreen = () => {
       <Layout style={{ flex: 1, padding: '5%', backgroundColor: 'white' }}>
         <BackButton />
         <Layout style={{ display: 'flex', flexDirection: 'column', backgroundColor: '#00000000', marginBottom: '25%', marginTop: '5%' }}>
-          <Text style={{ fontFamily: AppFontBold, textAlign: 'left' }} category="h3">Forgot your password</Text>
-          <Text style={{ color: '#8F9BB3'}} category="s1">Please enter your email address below to receive your password reset instructions. </Text>
+          <Text style={{ fontFamily: AppFontBold, textAlign: 'left' }} category="h3">
+            {i18n.t(TRANSLATIONS_KEY.FORGOT_SCREEN_TITLE).toString()}
+          </Text>
+          <Text style={{ color: '#8F9BB3'}} category="s1">
+            {i18n.t(TRANSLATIONS_KEY.FORGOT_SCREEN_SUB_TITLE).toString()}
+          </Text>
         </Layout>
 
 
@@ -32,8 +40,8 @@ const DocumentScreen = () => {
           style={{ backgroundColor: '#ffffff', borderRadius: 10, marginBottom: '3%' }}
           size='large'
           onChangeText={(e) => setEmail(e)}
-          label={() => <Text style={{ fontSize: 15, marginBottom: '2%' }} category='s2'>Email</Text>}
-          placeholder='Enter your email'
+          label={() => <Text style={{ fontSize: 15, marginBottom: '2%' }} category='s2'>{i18n.t(TRANSLATIONS_KEY.FORGOT_EMAIL_TAG).toString()}</Text>}
+          placeholder={i18n.t(TRANSLATIONS_KEY.FORGOT_EMAIL_PLACEHOLDER).toString()}
         />
 
         <Button
@@ -64,10 +72,10 @@ const DocumentScreen = () => {
             shadowRadius: 13.16,
             elevation: 10,
           }}>
-          {() => <Text style={{ fontFamily: AppFontBold,color: 'white', fontSize: 18 }}>Sent</Text>}
+          {() => <Text style={{ fontFamily: AppFontBold,color: 'white', fontSize: 18 }}>{i18n.t(TRANSLATIONS_KEY.FORGOT_SEND).toString()}</Text>}
           </Button>
         <Layout style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', backgroundColor: '#00000000' }}>
-          <Text onPress={() => navigation.navigate("Login")} style={{ color: '#41d5fb' }}>Back to Sign in</Text>
+          <Text onPress={() => navigation.navigate("Login")} style={{ color: '#41d5fb' }}>{i18n.t(TRANSLATIONS_KEY.FORGOT_BACK_TO_SIGN).toString()}</Text>
         </Layout>
 
       </Layout>
